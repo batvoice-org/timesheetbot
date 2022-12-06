413f7eeeac1f7cf2fd08b68080750cc1cb8dbd1a
Corentin Giraud <corentin@giraud.dev>
2022-12-06 14:00:26 +0100

    Disbale gunicorn access-logfile

--- prod_launch_web_interface.sh
+++ prod_launch_web_interface.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source /env/bin/activate
-gunicorn timesheetbot.wsgi:application --bind 0.0.0.0:8000 --workers 2 --timeout 900 --access-logfile -
+gunicorn timesheetbot.wsgi:application --bind 0.0.0.0:8000 --workers 2 --timeout 900 -
